d069f06d2d312f9000d770cbddf49808e1764373
Harrison Taylor <h.taylor@project89.org>
2015-04-06 16:11:18 -0400

    Worked a lot on CRM and probably ignored git pushing more than I should have

---
+++ Rolodex.rb
@@ -0,0 +1,48 @@
+class Rolodex
+
+  def initialize
+    @contacts = []
+    @id = 1000
+  end
+
+  def contacts
+    @contacts
+  end
+
+  def display_all
+    @contacts.each {|x| all_contacts[].push(x)}
+  end
+
+  def find(contact_id)
+    @contacts.select do |contact|
+      contact.id == contact_id
+    end
+  end
+
+  def find_by(contact_info)
+  end
+
+  def modify(modify_id, new_first, new_last, new_email, new_note)
+    @contacts.select do |modify|
+      modify.id == modify_id
+      modify.first_name = new_first
+      modify.last_name = new_last
+      modify.email = new_email
+      modify.note = new_note
+    end
+  end
+
+  def delete(delete_id)
+    @contacts.select do |delete|
+      delete.id == delete_id
+      @contacts.delete(delete.id)
+    end
+  end
+
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+
+end
---
+++ crm.rb
@@ -0,0 +1,114 @@
+require_relative "Contact"
+require_relative "Rolodex"
+#also load 'contact'
+#also require './contact'
+
+
+class CRM
+
+  def initialize(name)
+    @name = name
+    @rolodex = Rolodex.new
+  end
+
+  def self.run(name)
+    crm = new(name)
+    crm.main_menu
+  end
+
+  def print_main_menu
+    puts "[1] Add a new contact"
+    puts "[2] Modify an existing contact"
+    puts "[3] Delete a contact"
+    puts "[4] Display a contact"
+    puts "[5] Display all contacts"
+    puts "[6] Display an attribute"
+    puts "[7] Exit"
+    puts "Enter a number: "
+  end
+
+  def main_menu
+        print_main_menu
+        user_selected = gets.chomp.to_i
+        puts "\e[H\e[2J"
+        call_selection(user_selected)
+
+  end
+
+  def call_selection(user_selected)
+    add_new_contact if user_selected == 1
+    modify_existing_contact if user_selected == 2
+    delete_contact if user_selected == 3
+    display_contact if user_selected == 4
+    display_all if user_selected == 6
+    display_attribute if user_selected == 6
+    exit if user_selected == 7
+  end
+
+#Build disp contact before modify contact, it is simpler and contains building blocks for modify
+  def display_contact
+    print "Input contact ID"
+    contact_id = gets.chomp.to_i
+    contact = @rolodex.find(contact_id)
+    puts "#{contact}" + "\n \n"
+    puts "Push Return to continue"
+    button = gets.chomp
+    puts "\e[H\e[2J"
+    main_menu
+
+  end
+
+  def display_all
+    all_contacts = @rolodex.display_all
+    puts all_contacts
+    puts "Press Return to continue"
+    button = gets.chomp
+    main_menu
+  end
+
+  def add_new_contact
+    print "Enter First Name: "
+        first_name = gets.chomp
+    print "Enter Last Name: "
+        last_name = gets.chomp
+    print "Enter Email Address: "
+        email = gets.chomp
+    print "Enter a Note: "
+        note = gets.chomp
+    contact = Contact.new(first_name, last_name, email, note)
+    @rolodex.add_contact(contact)
+    puts "\e[H\e[2J"
+    main_menu
+  end
+
+  def modify_existing_contact
+    puts "Input contact ID"
+    modify_id = gets.chomp.to_i
+        print "Enter First Name: "
+        new_first = gets.chomp
+        print "Enter Last Name: "
+        new_last = gets.chomp
+        print "Enter Email Address: "
+        new_email = gets.chomp
+        print "Enter a Note: "
+        new_note = gets.chomp
+        @rolodex.modify(modify_id, new_first, new_last, new_email, new_note)
+        puts "\e[H\e[2J"
+        main_menu
+
+  end
+
+  def delete_contact
+    puts "Enter contact ID"
+    delete_id = gets.chomp.to_i
+    @rolodex.delete(delete_id)
+    puts "\e[H\e[2J"
+    main_menu
+  end
+
+  def exit
+    puts "Goodbye!"
+  end
+end
+
+CRM.run("robots")
